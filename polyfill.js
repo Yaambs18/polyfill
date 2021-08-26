9c63fc671b65929cf0e5a1ea06fb69cb0593d341
Yaambs18 <64511677+Yaambs18@users.noreply.github.com>
2021-08-26 17:13:06 +0530

    Add files via upload

---
+++ polyfill.js
@@ -0,0 +1,22 @@
+let name = {
+    firstname: "Yansh",
+    lastname: "Bhardwaj"
+  }
+  
+  let printName = function (hometown, state, country) {
+    console.log(this.firstname + " " + this.lastname + " , " + hometown + ", " + state + ", " + country);
+  }
+  
+  let printMyName = printName.bind(name, "Aligarh", "UP");
+  printMyName( "India");
+  
+  Function.prototype.mybind = function(...args){
+    let obj = this,
+      params = args.slice(1);
+    return function (...args2) {
+      obj.apply(args[0], [...params, ...args2]);
+    }
+  }
+  
+  let printMyName2 = printName.mybind(name, "Aligarh", "UP");
+  printMyName2( "India");
